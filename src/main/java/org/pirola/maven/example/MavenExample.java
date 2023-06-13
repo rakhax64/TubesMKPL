082cf8347657bc321d7cb752647adc00a5b1a6af
rakhax64 <95220908+rakhax64@users.noreply.github.com>
2023-06-13 16:47:02 +0700

    perubahan

--- src/main/java/org/pirola/maven/example/MavenExample.java
+++ src/main/java/org/pirola/maven/example/MavenExample.java
@@ -62,4 +62,7 @@ public final class MavenExample {
     public static String getGreeting() {
         return "Hello World!!!";
     }
+    public static String getGreeting() {
+        return "Hello World!!!";
+    }
 }
